803b9f98f0de6c21e7e02168ff15fdb6e6c021e0
Youssef-Yb07 <youssef.bouichenade@gmail.com>
2024-05-08 10:11:58 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask 
+import Flask, flash, request, redirect, url_for
+app = Flask(__name__)
+import os
+import resume_parser
+@app.route('/hello')
+def hello():
+    return 'Hello, World!'
+@app.route('/upload',methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        print(file)
+        new_path = os.path.join('./', file.filename)
+        file.save(new_path)
+        return resume_parser.extract_all_data(new_path)
+    return 'Please upload your file'
