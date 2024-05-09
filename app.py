6cf6a5a7ee097ecf022dc597e4500eeb69f4cf41
Youssef-Yb07 <youssef.bouichenade@gmail.com>
2024-05-09 09:22:03 +0100

    add years of experience parsing from cv

--- app.py
+++ app.py
@@ -1,25 +1,23 @@
 from flask import Flask, flash, request, redirect, url_for
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS,cross_origin
 app = Flask(__name__)
-import os
-import resume_parser
-
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
-
-
+import os
+import resume_parser
 @app.route('/hello')
 @cross_origin()
 def hello():
     return 'Hello, World!'
 @app.route('/upload',methods=['POST'])
-@cross_origin()
+@cross_origin() 
 def upload_file():
     if request.method == 'POST':
         file = request.files['file']
         print(file)
-        new_path = os.path.join('./', file.filename)
+        new_path = os.path.join('./files/', file.filename)
         file.save(new_path)
-        return resume_parser.extract_all_data(new_path)
+        response = resume_parser_v2.extract_all_data(new_path)
+        os.remove(new_path)
+        return response
     return 'Please upload your file'
-
